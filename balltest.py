63d4ea0babeaa7af86eae66374d80c9f92c8b1a1
Ariel Friedman <ariel.f2002@gmail.com>
2018-05-05 16:22:36 +0300

    changed the movement system. can now collision with walls correctly.

--- balltest.py
+++ balltest.py
@@ -3,59 +3,67 @@ import math
 from constants import * #this holds all the variables and their setters
 pygame.init()
 BACKGROUND_COLOR = (0,0,0)
-def move(ball, ang, speed, blocks):
-    rang = math.radians(ang)
-    move_x = round(speed*math.cos(rang))
-    move_y = round(speed*math.sin(rang))
-    if(move_x>move_y):
-        counter = move_x  # I need to keep the value of move_x but i also need to decrease it so here is a counter
-        while counter!=0:
-            for block in blocks:
-                if collide(ball,block):
-                    print 'stop!'
-                    ball['x'] = int(round(ball['x']))
-                    ball['y'] = int(round(ball['y']))
-                    return block['id']
-            ball['x']+=1
-            ball['y']+=move_y/move_x
-            counter-=1
-    else:
-        counter = move_y  # I need to eep the value of move_x but i also need to decrease it so here is a counter
-        while counter != 0:
-            for block in blocks:
-                if collide(ball, block):
-                    ball['x'] = int(round(ball['x']))
-                    ball['y'] = int(round(ball['y']))
-                    return block['id']
-            ball['x'] += move_x/move_y
-            ball['y'] += 1
-            counter -= 1
+def move(ball,blocks,players):
+    #this moves the players
+    for player in players:
+        if player['movement'] == MOVE_DOWN:
+            player['y']+=5
+        if player['movement'] == MOVE_UP:
+            player['y']-=5
+    rang = math.radians(ball['ang'])
+    move_x = int(round(ball['speed']*math.cos(rang)))
+    move_y = int(round(ball['speed']*math.sin(rang)))
+    #there are two different loops that seem identical, but they are here so that the movement feels more natural
+    counter = abs(move_y)  # I need to keep the value of move_x but i also need to decrease it so here is a counter
+    while counter != 0:
+        id = 0  # this counts the place of the object in the array
+        for block in blocks:
+            collided,direction = collide(ball, block) #this is saved so I can use the direction later
+            if collided:
+                ball['x'] = int(round(ball['x']-math.copysign(float(move_x)/move_y,move_x)))
+                ball['y'] = int(round(ball['y']-math.copysign(1,move_y)))
+                return id, direction
+            id += 1
+        ball['x'] += math.copysign(float(move_x)/move_y,move_x)
+        ball['y'] += math.copysign(1,move_y)
+        counter -= 1
     ball['x'] = int(round(ball['x']))
     ball['y'] = int(round(ball['y']))
-    return -1
-    '''if ball['x'] > 600:
-        ball['x'] = 600 - ball['r']
-    elif ball['x'] < 0:
-        ball['x'] = 0 + ball['r']
-    if ball['y'] > 600:
-        ball['y'] = 600 - ball['r']
-    elif ball['y'] < 0:
-        ball['y'] = 0 + ball['r']'''
+    return -1, "NaN"
+def simple_move(ball,speed = 0,ang = 0):
+    '''the difference between this movement method and the 'move' command is that this lacks collision detection'''
+    if speed==0:
+        speed = ball['speed']
+    if ang==0:
+        ang = ball['ang']
+    rang = math.radians(ang)
+    ball['x'] += round(speed*math.cos(rang))
+    ball['y'] += round(speed*math.sin(rang))
 def round(num):
     if num%1 >= 0.5:
-        return math.ceil(num)
-    return math.floor(num)
+        return int(math.ceil(num))
+    return int(math.floor(num))
+def near(num1,num2,difference):
+    '''checks if the first number is near the second number by the number provided.
+        for example: near(2,4,1) returns True because: (2<=4<=2+1 or 2-1<=4<=2) equals (False or True) equals True'''
+    return num1<=num2<=num1+difference or num1-difference<=num2<=num1
 def collide(ball,object):
-    '''ball_movement is a list containing the start and end point of the movement of the ball: [(start_x,start_ y),(end_x,end_y)]'''
+    '''returns a tuple containing a boolean (did the ball hit something?) and a string that represents direction of hit (x or y) '''
     if 'r' in object:
         return math.sqrt((math.fabs(ball['x']-object['x'])**2) + (math.fabs(ball['y']-object['y'])**2)) <= ball['r']
     else:
-        #print ball['x']+ball['r']==object['x'] , ball['x']-ball['r']==object['x']+object['w'] , ball['y']+ball['r']==object['y'] , ball['y']-ball['r']==object['y']+object['h']
-        #print 'up',ball['y']+ball['r']<=object['y'] and object['x'] <= ball['x'] <= object['x'] + object['w'], 'left:', ball['x']+ball['r']<=object['x']and object['y'] <= ball['y'] <= object['y'] + object['h']
-        if (ball['x']+ball['r']<=object['x'] or ball['x']-ball['r']<=object['x']+object['w']):
-            return object['y'] <= ball['y'] <= object['y'] + object['h']
-        if (ball['y']+ball['r']<=object['y'] or ball['y']-ball['r']<=object['y']+object['h']):
-            return object['x'] <= ball['x'] <= object['x'] + object['w']
+        if near(ball['x']+ball['r'],object['x'],2) or near(ball['x']-ball['r'],object['x']+object['w'],2):
+            return object['y'] <= ball['y'] <= object['y'] + object['h'] , 'x'
+        if near(ball['y']+ball['r'],object['y'],2) or near(ball['y']-ball['r'],object['y']+object['h'],2):
+            return object['x'] <= ball['x'] <= object['x'] + object['w'] , 'y'
+        return False,"NaN"
+def change_direction(ball,direction):
+    '''takes a ball object and also the direction of the hit (x or y)'''
+    if direction == 'x':
+        ball['speed']*=-1
+        ball['ang']*=-1
+    else:
+        ball['ang']*=-1
 def add_block(blocks,x,y,w,h,color):
     blocks.append({'x':x,'y':y,'w':w,'h':h,'color':color,'id':len(blocks)})
 def draw(screen, balls, blocks):
@@ -66,34 +74,35 @@ def draw(screen, balls, blocks):
         pygame.draw.rect(screen,obj['color'],(obj['x'],obj['y'],obj['w'],obj['h']),0)
     pygame.display.flip()
 def main():
+    screen = pygame.display.set_mode((600, 600))
+    set_screen_size(screen.get_width(), screen.get_height())
     TIMEREVENT = pygame.USEREVENT+1
-    ball1 = {'x':100,'y':100,'r':10,'color':(200,45,132),'speed':10,'ang':20}
-    ball2 = {'x':100,'y':150,'r':10,'color':(56,250,143),'speed':10,'ang':15}
-    balls = [ball2]
+    ball = {'x':100,'y':150,'r':10,'color':(56,250,143),'speed':10,'ang':20,'stuck':False}
+    balls = [ball]
+    player = {'x':screen_percent(5)[0],'y': 0,'w':25,'h':100,'color':random_color(),'id':"player",'movement':MOVE_STILL}
     blocks = [] #this holds the data of all the blocks (not including the walls). each blocks place in the list coressponds to its ID.
-    screen = pygame.display.set_mode((600,600))
-    pygame.time.set_timer(TIMEREVENT,100)
+    #set_screen_size(screen.get_width(),screen.get_height())
+    pygame.time.set_timer(TIMEREVENT,16)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                add_block(blocks,pos[0],pos[1],50,100,(132,43,11))
-            elif event.type == TIMEREVENT:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    player['movement'] = MOVE_UP
+                elif event.key == pygame.K_s:
+                    player['movement'] = MOVE_DOWN
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_s:
+                    player['movement'] = MOVE_STILL
+            if event.type == TIMEREVENT:
                 for ball in balls:
-                    object_hit_id = move(ball,ball['ang'],ball['speed'],blocks) #this is saved so that the object is removed from the game later
-                    if object_hit_id != -1:
-                        balls.remove(ball)
-                    if len(balls) == 0:
-                        print 'stop!'
-                        print ball1 , ball2, object_hit_id
-                        return 1
-                    draw(screen, balls, blocks)
+                    id,direction = move(ball,blocks+walls()+[player],[player]) #this is saved so that the object can removed from the game later
+                    if direction!="NaN":
+                        print direction
+                    if id != -1:
+                        change_direction(ball,direction)
+                    draw(screen, balls, blocks+[player])
     pygame.quit()
 main()
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
--- constants.py
+++ constants.py
@@ -1,7 +1,10 @@
 import random
 #all the constants
 screen_size = [0,0]
-colors = {'black':(0,0,0),'white':(255,255,255),'red':(255,0,0),'green':(0,255,0),'blue':(0,0,255)}
+colors = {'black':(0,0,0),'white':(255,255,255),'red':(255,0,0),'green':(0,255,0),'blue':(0,0,255),'background': (0,0,0)}
+MOVE_STILL = 0 #this is for checking if thep layer is not moving.
+MOVE_UP = 1 #this is for movement up
+MOVE_DOWN = 2 #and this one is down
 #setters for the constants
 def set_screen_size(width,height):
     screen_size[0] = width
@@ -9,11 +12,18 @@ def set_screen_size(width,height):
 def screen_percent(percent, **from_end):
     '''takes a percent of the screen and return the amount of pixels. if from_end = True, the it return the amount of pixels from end'''
     if from_end:
-        print "TODO: add this."
+        backwards = 100-percent #calcualtes the percent from end (i.e. 90% becomes 10%)
+        return float(screen_size[0]*backwards)/100 , float(screen_size[1]*backwards)/100
     else:
-        return (screen_size[0]*percent)/100,(screen_size[1]*percent)/100
-def add_color(name_of_color,color,**is_opaque)
-    colors[name_of_color]=color
+        return float(screen_size[0]*percent)/100 , float(screen_size[1]*percent)/100
+def walls():
+    '''this return a list of dictionaries that represent the bounds of the game'''
+    up = {'x': 0,'y': -50,'w': screen_size[0] ,'h':50,'color': colors['background'],'id':'up'}
+    down = {'x': 0,'y': screen_size[1],'w': screen_size[0] ,'h':50,'color': colors['background'],'id':'down'}
+    left = {'x': -50,'y': 0,'w': 50 ,'h':screen_size[1],'color': colors['background'],'id':'left'}
+    right = {'x': screen_size[0] , 'y': 0, 'w': 50 , 'h': screen_size[1], 'color': colors['background'], 'id': 'right'}
+    return [up,down,left,right]
+def set_color(name_of_color,color):
+    colors[name_of_color] = color
 def random_color():
     return random.randrange(256),random.randrange(256),random.randrange(256)
-def
